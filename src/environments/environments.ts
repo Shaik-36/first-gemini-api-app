363c114beba7e8c720bc8ecbedae8da32abc9dd6
imamshaik305 <imamshaik305@gmail.com>
2024-12-28 16:26:37 +0000

    Initial Commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyDOiiIBklZWWyXSAPj6cSHOUw-pqgGhvNQ',
+      authDomain: 'easy-chat-18200.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'easy-chat-18200',
+      storageBucket: 'easy-chat-18200.firebasestorage.app',
+      messagingSenderId: '626288285664',
+      appId: '1:626288285664:web:5fbc4f3a8c00ed211d3d85',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyC_4Ei7GToK5cEIdhDtmZeTbDWPI1Oe03s',
+  debug_token: ''
+};
